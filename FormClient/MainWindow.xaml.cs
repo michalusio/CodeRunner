c9f2c6a66b3d4a4fc027da153155d9ab59362d99
michalusio <micha_i@interia.pl>
2020-10-14 00:14:34 +0200

    Changing the Client to WPF, as it allows for not having an old handle for the form.

---
+++ FormClient/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Shell;
+
+namespace FormClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
